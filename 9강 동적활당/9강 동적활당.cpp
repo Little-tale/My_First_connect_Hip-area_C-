2a2f540800f3461078b5c3af2c1cd01bbb1ceaf6
shallytwince <you3192001@gmail.com>
2022-11-27 00:02:24 +0900

    초기 커밋.

---
+++ 9강 동적활당/9강 동적활당.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+//메모리 영역
+//힙 영역(동적할당)
+
+int main()
+{
+	int* pInt = nullptr;
+	int input = 0;
+	scanf_s("%d",&input);
+	if (100 == input)
+	{
+		pInt = (int*)malloc(100);
+	}
+	if (nullptr != pInt)
+	{
+		free(pInt);
+	}
+
+
+	//가변 배열
+	int arr[10] = {};
+	
+
+
+	return 0;
+}
---
+++ 9강 동적활당/arr.h
@@ -0,0 +1,6 @@
+
+
+typedef struct _tapArr
+{
+
+}tArr//가변배열 자료형;
